eb6196b317ca1859cc6e9864d9240534c7cf0087
Jhon <jdcastaneda52@gmail.com>
2022-02-25 10:43:35 -0500

    Navigator dependencies and initial config

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>POKEDEX</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>POKEDEX</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
