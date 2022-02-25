55426553544a2db46dfeacbd59595e7ebfcaa11d
Jhon <jdcastaneda52@gmail.com>
2022-02-25 15:36:22 -0500

    Consult Pokemon's data from api

---
+++ src/api/pokemon.js
@@ -0,0 +1,22 @@
+import { API_HOST } from "../utils/constants";
+
+export async function getPokemonsApi() {
+  try {
+    const url = `${API_HOST}/pokemon?limit=20&offset=0`;
+    const response = await fetch(url);
+    const result = response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getDetailPokemonByUrlApi(url) {
+  try {
+    const response = await fetch(url);
+    const result = response.json();
+    return result
+  } catch (error) {
+    throw error;
+  }
+}
--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,11 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { getDetailPokemonByUrlApi, getPokemonsApi } from "../api/pokemon";
 
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+  console.log(pokemons);
+  useEffect(() => {
+    (async () => {
+      loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      const pokemonsArray = [];
+      for await (const pokemon of response.results) {
+        const pokemonDetails = await getDetailPokemonByUrlApi(pokemon.url);
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name:pokemonDetails.name,
+          type:pokemonDetails.types[0].type.name,
+          order:pokemonDetails.order,
+          image:pokemonDetails.sprites.other['official-artwork'].front_default
+        });
+      }
+      setPokemons([...pokemons,...pokemonsArray]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
     </SafeAreaView>
-  )
+  );
 }
