da19efe3115fe2406929c77bf92ba6973b0066a4
Jhon <jdcastaneda52@gmail.com>
2022-02-25 10:56:57 -0500

    Tab navigation

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import "react-native-gesture-handler";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./src/navigation/Navigation";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>POKEDEX</Text>
-      </View>
+      <Navigation/>
     </NavigationContainer>
   );
 }
