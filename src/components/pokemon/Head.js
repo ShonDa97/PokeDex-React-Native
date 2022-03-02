4a321124c802998a908fae83421330aa5121c4c0
Jhon <jdcastaneda52@gmail.com>
2022-03-02 14:26:47 -0500

    Head component to contains image, name and order of pokemon

---
+++ src/components/pokemon/Head.js
@@ -0,0 +1,65 @@
+import { View, Text, Image, StyleSheet ,SafeAreaView} from "react-native";
+import React from "react";
+import getColorByPokemonType from "../../utils/getColorByPokemonType";
+
+export default function Head({ name, image, type, order }) {
+  const color = getColorByPokemonType(type);
+  const bgStyle = { backgroundColor: color, ...styles.bg };
+
+  return (
+    <>
+      <View style={bgStyle} />
+      <SafeAreaView style={styles.content}>
+        <View style={styles.header}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.order}>#{`${order}`.padStart(3,0)}</Text>
+        </View>
+        <View style={styles.contentImg}>
+          <Image source={{ uri: image }} style={styles.image} />
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  bg: {
+    width: "100%",
+    height: 400,
+    position: "absolute",
+    borderBottomEndRadius: 300,
+    borderBottomLeftRadius: 300,
+    transform:[{scaleX:2}]
+  },
+  content:{
+    marginHorizontal:20,
+    marginTop:30
+  },
+  header:{
+    textTransform:"capitalize",
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:"center",
+    paddingTop:40
+  },
+  name:{
+    color:"#fff",
+    fontWeight:"bold",
+    fontSize:27
+  },
+  order:{
+    color:"#fff",
+    fontWeight:"bold",
+  },
+  contentImg: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    top: 30,
+  },
+  image: {
+    width: 250,
+    height: 300,
+    resizeMode:"contain"
+  },
+});
--- src/navigation/PokedexNavigation.js
+++ src/navigation/PokedexNavigation.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
-import Pokedex from '../screens/Pokedex'
-import Pokemon from '../screens/Pokemon'
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Pokedex from "../screens/Pokedex";
+import Pokemon from "../screens/Pokemon";
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function PokedexNavigation() {
   return (
     <Stack.Navigator>
-        <Stack.Screen name='Pokedex' component={Pokedex} options={{title:"",headerShown:false}}/>
-        <Stack.Screen name='Pokemon' component={Pokemon}/>
+      <Stack.Screen
+        name="Pokedex"
+        component={Pokedex}
+        options={{ title: "", headerShown: false }}
+      />
+      <Stack.Screen
+        name="Pokemon"
+        component={Pokemon}
+        options={{ title: "", headerTransparent: true }}
+      />
     </Stack.Navigator>
-  )
+  );
 }
